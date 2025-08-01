3963a7b4b8f0b3b4b93ca523aaf35bf08e18446d
Hans-J. Schmid <knock@myopendoor.de>
2025-08-01 23:03:43 +0200

    ch13

--- 13/code.clj
+++ 13/code.clj
@@ -1,4 +1,16 @@
-(ns were-creatures)
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Creating and Extending Abstractions with Multimethods, Protocols, and Records
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(ns code)
+
+;; ;;;;;;;;;;;;
+;; Polymorphism
+;; ;;;;;;;;;;;;
+
+;; ;;;;;;;;;;;;
+;; Multimethods
+;; ;;;;;;;;;;;;
 
 (defmulti full-moon-behavior (fn [were-creature] (:were-type were-creature)))
 
@@ -12,11 +24,11 @@
 
 (full-moon-behavior {:were-type :wolf
                      :name "Rachel from next door"})
-; => "Rachel from next door will howl and murder"
+; "Rachel from next door will howl and murder"
 
 (full-moon-behavior {:name "Andy the baker"
                      :were-type :simmons})
-; => "Andy the baker will encourage people and sweat to the oldies"
+; "Andy the baker will encourage people and sweat to the oldies"
 
 (defmethod full-moon-behavior nil
   [were-creature]
@@ -24,7 +36,7 @@
 
 (full-moon-behavior {:were-type nil
                      :name "Martin the nurse"})
-; => "Martin the nurse will stay at home and eat ice cream"
+; "Martin the nurse will stay at home and eat ice cream"
 
 (defmethod full-moon-behavior :default
   [were-creature]
@@ -32,130 +44,136 @@
 
 (full-moon-behavior {:were-type :office-worker
                      :name "Jimmy from sales"})
-; => "Jimmy from sales will stay up all night fantasy footballing"
+; "Jimmy from sales will stay up all night fantasy footballing"
 
+(comment
+  ; "extending defmulti"
+  (ns random-namespace
+    (:require [code]))
 
-(ns random-namespace
-  (:require [were-creatures]))
-(defmethod were-creatures/full-moon-behavior :bill-murray
-  [were-creature]
-  (str (:name were-creature) " will be the most likeable celebrity"))
-(were-creatures/full-moon-behavior {:name "Laura the intern" 
-                                    :were-type :bill-murray})
-; => "Laura the intern will be the most likeable celebrity"
+  (defmethod code/full-moon-behavior :bill-murray
+    [were-creature]
+    (str (:name were-creature) " will be the most likeable celebrity"))
 
-(ns user)
-(defmulti types (fn [x y] [(class x) (class y)]))
+  ; "Laura the intern will be the most likeable celebrity"
+  (code/full-moon-behavior {:name "Laura the intern"}
+                           :were-type :bill-murray))
+
+; multiple parameters
+(defmulti  types (fn [x y] [(class x) (class y)]))
 (defmethod types [java.lang.String java.lang.String]
-  [x y]
+  [_x _y]
   "Two strings!")
 
 (types "String 1" "String 2")
-; => "Two strings!"
+; "Two strings!"
 
-(ns data-psychology)
+;; ;;;;;;;;;
+;; Protocols
+;; ;;;;;;;;;
+
 (defprotocol Psychodynamics
   "Plumb the inner depths of your data types"
   (thoughts [x] "The data type's innermost thoughts")
   (feelings-about [x] [x y] "Feelings about self or other"))
 
-(feelings-about [x] [x & others])
-
 (extend-type java.lang.String
   Psychodynamics
-  (thoughts [x] (str x " thinks, 'Truly, the character defines the data type'")
-    (feelings-about
-     ([x] (str x " is longing for a simpler way of life"))
-     ([x y] (str x " is envious of " y "'s simpler way of life")))))
+  (thoughts [x] (str x " thinks, 'Truly, the character defines the data type'"))
+  (feelings-about
+    ([x] (str x " is longing for a simpler way of life"))
+    ([x y] (str x " is envious of " y "'s simpler way of life"))))
 
 (thoughts "blorb")
-; => "blorb thinks, 'Truly, the character defines the data type'"
+; "blorb thinks, 'Truly, the character defines the data type'"
 
 (feelings-about "schmorb")
-; => "schmorb is longing for a simpler way of life"
+; "schmorb is longing for a simpler way of life"
 
 (feelings-about "schmorb" 2)
-; => "schmorb is envious of 2's simpler way of life"
+; "schmorb is envious of 2's simpler way of life"
 
+; adding default type (= java.lang.Object)
 (extend-type java.lang.Object
   Psychodynamics
-  (thoughts [x] "Maybe the Internet is just a vector for toxoplasmosis")
+  (thoughts [_x] "Maybe the Internet is just a vector for toxoplasmosis")
   (feelings-about
-    ([x] "meh")
-    ([x y] (str "meh about " y))))
+    ([_x] "meh")
+    ([_x y] (str "meh about " y))))
 
 (thoughts 3)
-; => "Maybe the Internet is just a vector for toxoplasmosis"
+; "Maybe the Internet is just a vector for toxoplasmosis"
 
 (feelings-about 3)
-; => "meh"
+; "meh"
 
 (feelings-about 3 "blorb")
-; => "meh about blorb"
+; "meh about blorb"
 
 (extend-protocol Psychodynamics
   java.lang.String
-  (thoughts [x] "Truly, the character defines the data type")
+  (thoughts [_x] "Truly, the character defines the data type")
   (feelings-about
-    ([x] "longing for a simpler way of life")
-    ([x y] (str "envious of " y "'s simpler way of life")))
-  
+    ([_x] "longing for a simpler way of life")
+    ([_x y] (str "envious of " y "'s simpler way of life")))
+
   java.lang.Object
-  (thoughts [x] "Maybe the Internet is just a vector for toxoplasmosis")
+  (thoughts [_x] "Maybe the Internet is just a vector for toxoplasmosis")
   (feelings-about
-    ([x] "meh")
-    ([x y] (str "meh about " y))))
+    ([_x] "meh")
+    ([_x y] (str "meh about " y))))
 
 (ns were-records)
 (defrecord WereWolf [name title])
 
 (WereWolf. "David" "London Tourist")
-; => #were_records.WereWolf{:name "David", :title "London Tourist"}
+; {:name "David", :title "London Tourist"}
 
 (->WereWolf "Jacob" "Lead Shirt Discarder")
-; => #were_records.WereWolf{:name "Jacob", :title "Lead Shirt Discarder"}
+; {:name "Jacob", :title "Lead Shirt Discarder"}
 
 (map->WereWolf {:name "Lucian" :title "CEO of Melodrama"})
-; => #were_records.WereWolf{:name "Lucian", :title "CEO of Melodrama"}
+; {:name "Lucian", :title "CEO of Melodrama"}
 
 (ns monster-mash
   (:import [were_records WereWolf]))
+
 (WereWolf. "David" "London Tourist")
-; => #were_records.WereWolf{:name "David", :title "London Tourist"}
+; {:name "David", :title "London Tourist"}
 
 (def jacob (->WereWolf "Jacob" "Lead Shirt Discarder"))
-(.name jacob) 
-; => "Jacob"
+(.name jacob)
+; "Jacob"
 
-(:name jacob) 
-; => "Jacob"
+(:name jacob)
+; "Jacob"
 
-(get jacob :name) 
-; => "Jacob"
+(get jacob :name)
+; "Jacob"
 
 (= jacob (->WereWolf "Jacob" "Lead Shirt Discarder"))
-; => true
+; true
 
 (= jacob (WereWolf. "David" "London Tourist"))
-; => false
+; false
 
 (= jacob {:name "Jacob" :title "Lead Shirt Discarder"})
-; => false
+; false
 
 (assoc jacob :title "Lead Third Wheel")
-; => #were_records.WereWolf{:name "Jacob", :title "Lead Third Wheel"}
+; {:name "Jacob", :title "Lead Third Wheel"}
 
 (dissoc jacob :title)
-; => {:name "Jacob"} <- that's not a were_records.WereWolf
+; {:name "Jacob"} <- that's not a were_records.WereWolf
 
+; implement protocol via record
+(comment
+  (defprotocol WereCreature
+    (full-moon-behavior [x]))
 
-(defprotocol WereCreature
-  (full-moon-behavior [x]))
+  (defrecord WereWolf [name title]
+    WereCreature
+    (full-moon-behavior [_x] (str name " will howl and murder")))
 
-(defrecord WereWolf [name title]
-  WereCreature
-  (full-moon-behavior [x]
-    (str name " will howl and murder")))
-
-(full-moon-behavior (map->WereWolf {:name "Lucian" :title "CEO of Melodrama"}))
-; => "Lucian will howl and murder"
+  ; "Lucian will howl and murder"
+  (full-moon-behavior (map->WereWolf {:name "Lucian" :title "CEO of Melodrama"})))
